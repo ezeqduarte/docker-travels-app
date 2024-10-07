6c8a7ed3c3b6e05021dd0474224417f122564aff
ezeqduarte <ezeqduarte01@gmail.com>
2024-10-06 23:01:00 -0300

    feat: add new content into card carousel and refactor component

--- src/lib/components/home/card-carousel/CardCarousel.tsx
+++ src/lib/components/home/card-carousel/CardCarousel.tsx
@@ -8,23 +8,27 @@ const categoriesColors: any = {
     masividad: 'bg-[#ff4d00]',
 };
 
-const LabelsContainer = ({ categories }: any) => {
-    return (
-        <div className='absolute bottom-2 right-2 flex gap-2 text-xs'>
-            {categories.map((category: string) => (
-                <span className={`${categoriesColors[category]} px-3 rounded-lg`}>{category}</span>
-            ))}
-        </div>
-    );
-}
-
 export const CardCarousel = ({ item }: any) => {
     return (
-        <div
+        <article
             style={{ backgroundImage: `url(${item?.foto})` }}
-            className='bg-[image:var(--image-url)] bg-[#969696ab] bg-center bg-cover bg-blend-multiply w-full relative h-full rounded-xl'>
+            className='bg-[image:var(--image-url)] bg-[#969696ab] py-2 px-3 bg-center bg-cover bg-blend-multiply w-full relative h-full rounded-xl cursor-pointer'>
+            <div className="flex w-full justify-between items-center">
+                <h4 className="text-white font-bold text-[24px] ">{item.nombre}</h4>
+                <div className="text-white rounded-xl flex gap-2">
 
-            <LabelsContainer categories={item.categorias} />
-        </div>
+                    <span className='text-[24px]'>{'<3'}</span>
+                </div>
+            </div>
+            <div className="absolute bottom-7 right-4 text-white rounded-xl flex gap-2">
+
+                <span className='text-[24px]'>{item.valoracion}</span>
+            </div>
+            <div className='absolute bottom-2 right-2 flex gap-2 text-xs'>
+                {item.categorias.map((category: string) => (
+                    <span className={`${categoriesColors[category]} px-3 rounded-lg`}>{category}</span>
+                ))}
+            </div>
+        </article>
     );
 };
