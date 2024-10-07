bbe5ea641e7173fb14e8d677a505e6b649e028b4
ezeqduarte <ezeqduarte01@gmail.com>
2024-10-06 23:28:05 -0300

    feat: mock navbar buttons

--- src/lib/components/layout/navbar/Navbar.tsx
+++ src/lib/components/layout/navbar/Navbar.tsx
@@ -1,8 +1,23 @@
+import { HomeIcon } from "@/lib/icons";
 
-export const Navbar = async () => {
+const navbarLinks = [1, 2, 3, 4, 5];
+
+const ButtonNavbar = ({ index }: { index: number }) => {
     return (
-        <nav className="h-16 fixed bottom-0 w-full left-0 bg-slate-400 rounded-t-[20px] flex flex-col p-3 items-center">
-           <p>a</p>
+        <button className={`${index === 2 ? 'bg-white rounded-full border-gray-300 border' : ''} p-4`}>
+            <HomeIcon />
+        </button>
+    );
+}
+
+export const Navbar = () => {
+    return (
+        <nav className="h-16 fixed bottom-0 w-full left-0 bg-slate-400 rounded-t-[20px] p-3 flex justify-center items-center">
+            <div className="grid grid-cols-5 pb-4 gap-7 justify-items-center">
+                {navbarLinks.map((button, index) => (
+                    <ButtonNavbar key={index} index={index} /> 
+                ))}
+            </div>
         </nav>
-    )
+    );
 }
