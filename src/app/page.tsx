c0b29bbfb1b7dcb74f05ef9e37881cb98626be8f
ezeqduarte <ezeqduarte01@gmail.com>
2024-10-06 23:04:43 -0300

    feat: mock two carousels into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { Carousel } from "@/lib/components";
 
 export default function Home() {
   return (
-    <div className="grow bg-white h-full py-[80px] px-[30px] ">
+    <div className="grow bg-white h-full py-[80px] px-[30px] flex flex-col gap-5 ">
+        <Carousel title="Populares" slides={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} options={{dragFree: true}}></Carousel>
         <Carousel title="Populares" slides={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} options={{dragFree: true}}></Carousel>
     </div>
   );
