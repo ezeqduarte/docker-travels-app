9923a07209ffcea68a27bb9689afe60e8a1c4487
ezeqduarte <ezeqduarte01@gmail.com>
2024-10-06 23:09:29 -0300

    fix: refactor styles into header and navbar components

--- src/lib/components/layout/navbar/Navbar.tsx
+++ src/lib/components/layout/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 
 export const Navbar = async () => {
     return (
-        <nav className="h-16 absolute bottom-0 w-screen left-0 bg-slate-400 rounded-t-[20px] flex flex-col p-3 items-center">
+        <nav className="h-16 fixed bottom-0 w-full left-0 bg-slate-400 rounded-t-[20px] flex flex-col p-3 items-center">
            <p>a</p>
         </nav>
     )
