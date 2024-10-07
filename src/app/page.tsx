d93a8f4e30a8e6caa758380ec0630ce87f85dc13
ezeqduarte <ezeqduarte01@gmail.com>
2024-10-06 23:29:45 -0300

    fix: change title into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grow bg-white min-h-full py-[80px] px-[30px] flex flex-col gap-5 ">
         <Carousel title="Populares" slides={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} options={{dragFree: true}}></Carousel>
-        <Carousel title="Populares" slides={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} options={{dragFree: true}}></Carousel>
+        <Carousel title="Recomendadas" slides={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} options={{dragFree: true}}></Carousel>
     </div>
   );
 }
